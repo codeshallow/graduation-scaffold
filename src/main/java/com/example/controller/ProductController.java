fdab77897ca46550eecf1ab04b5b1c6dac89a880
codeshallow <1272178260@qq.com>
2022-01-31 01:53:46 +0800

    product 产品导出功能开发

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -1,13 +1,24 @@
 package com.example.controller;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import com.example.common.Result;
 import com.example.entity.Product;
+import com.example.entity.User;
 import com.example.service.ProductService;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/product")
@@ -44,6 +55,33 @@ public class ProductController {
     @GetMapping("/page")
     public Result<Page<Product>> findPage(@RequestParam(required = false, defaultValue = "1") Integer pageNum, @RequestParam(required = false, defaultValue = "10") Integer pageSize) {
         return Result.success(productService.findPage(pageNum, pageSize));
+    }
+
+    @GetMapping("/export")
+    public void export(HttpServletResponse response) throws IOException {
+
+        List<Map<String, Object>> list = CollUtil.newArrayList();
+
+        List<Product> all = productService.findAll();
+        for (Product product : all) {
+            Map<String, Object> row1 = new LinkedHashMap<>();
+            row1.put("名称", product.getName());
+            row1.put("内容", product.getContent());
+            list.add(row1);
+        }
+
+        // 2. 写excel
+        ExcelWriter writer = ExcelUtil.getWriter(true);
+        writer.write(list, true);
+
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        String fileName = URLEncoder.encode("产品表", "UTF-8");
+        response.setHeader("Content-Disposition", "attachment;filename=" + fileName + ".xlsx");
+
+        ServletOutputStream out = response.getOutputStream();
+        writer.flush(out, true);
+        writer.close();
+        IoUtil.close(System.out);
     }
 
 }
